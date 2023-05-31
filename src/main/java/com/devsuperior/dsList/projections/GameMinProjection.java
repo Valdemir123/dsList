e8d9c84301284a4bc197e36467df856755e3bf5f
Valdemir PS <valpersil@gmail.com>
2023-05-31 11:29:03 -0300

    Homolog OK

--- src/main/java/com/devsuperior/dsList/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dsList/projections/GameMinProjection.java
@@ -4,10 +4,9 @@ public interface GameMinProjection {
 
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
-	
 	
 }
